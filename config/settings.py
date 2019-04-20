958c7eabcb68e3503dcc4fd1dc273f9f7b010617
Sean xia <sean.xia.xu@gmail.com>
2019-04-20 18:11:31 +0800

    basic config finish

--- config/settings.py
+++ config/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -37,6 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #第三方
+    'ckeditor',
+    'ckeditor_uploader',
+    #自己的app
+    'home.apps.IgcaeConfig',
+    'blog.apps.BlogConfig',
+    'product.apps.ProductConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +126,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+CKEDITOR_UPLOAD_PATH = "uploads/"
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

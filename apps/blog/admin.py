812c3e5f31c7c3449c5dd5e0360571bc36b0e47e
Sean xia <sean.xia.xu@gmail.com>
2019-04-28 20:45:26 +0800

    backend finished

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+
+
+@admin.register(Post)
+class BlogAdmin(admin.ModelAdmin):
+	list_display = ('title', 'slug', 'publish', 'status')
+	list_filter = ('status', 'created', 'publish')
+    # list_editable = ['status']
+	search_fields = ('title', 'content')
+	prepopulated_fields = {'slug': ('title',)}
+	# raw_id_fields = ('author',)
+	date_hierarchy = 'publish'
+	ordering = ('status', 'publish')
